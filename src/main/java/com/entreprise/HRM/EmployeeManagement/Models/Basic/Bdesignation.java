9286d5676ba633381fd6ff10a81d67188da8825d
biruksolomon <biruksolomonmoges@gmail.com>
2025-02-13 11:15:33 -0400

    some bdesignation update

--- src/main/java/com/entreprise/HRM/EmployeeManagement/Models/Basic/Bdesignation.java
+++ src/main/java/com/entreprise/HRM/EmployeeManagement/Models/Basic/Bdesignation.java
@@ -14,7 +14,6 @@ public class Bdesignation {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-
     private String title;
 
     @OneToMany(mappedBy = "bdesignation", cascade = CascadeType.ALL)
