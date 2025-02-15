514e0ac2e5bb30d0f2e52d1df224db9f08478d2f
biruksolomon <biruksolomonmoges@gmail.com>
2025-02-15 08:04:00 -0400

    adding salary controller

--- src/main/java/com/entreprise/HRM/EmployeeManagement/conttest/payrollcontroller.java
+++ src/main/java/com/entreprise/HRM/EmployeeManagement/conttest/payrollcontroller.java
@@ -1,4 +1,23 @@
 package com.entreprise.HRM.EmployeeManagement.conttest;
 
+import com.entreprise.HRM.EmployeeManagement.Models.payroll.salary_structure;
+import com.entreprise.HRM.EmployeeManagement.service.salaryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class payrollcontroller {
+    @Autowired
+    private salaryService salaryservice;
+
+    @PutMapping("salary/{id}")
+    public ResponseEntity<Object> addingsalary_structure(@PathVariable long id, @RequestBody salary_structure sal) {
+        return new ResponseEntity<>(salaryservice.addSalary(id, sal), HttpStatus.OK);
+
+    }
 }
